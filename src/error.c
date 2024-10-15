aac16319dfc07e973746085558e2e433aeca7132
Sanidhyafeaturist <141141037+Sanidhyafeaturist@users.noreply.github.com>
2024-10-15 15:54:01 +0530

    Update error.c

--- src/error.c
+++ src/error.c
@@ -173,17 +173,6 @@ raise_element_not_found( void ) {
                NULL );
 }
 
-void
-raise_error( enum stumpless_error_id id,
-             const char *message,
-             int code,
-             const char *code_type ) {
-  last_error.id = id;
-  last_error.message = message;
-  last_error.code = code;
-  last_error.code_type = code_type;
-  error_valid = true;
-}
 
 void
 raise_file_open_failure( void ) {
