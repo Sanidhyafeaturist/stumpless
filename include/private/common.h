ae14884acef01b6908d70cce3682be72d287f093
Sanidhyafeaturist <141141037+Sanidhyafeaturist@users.noreply.github.com>
2024-10-15 16:09:15 +0530

    Update common.h

--- include/private/common.h
+++ include/private/common.h
@@ -4,17 +4,4 @@
 #include "error.h"
 
 
-void
-raise_error( enum stumpless_error_id id,
-             const char *message,
-             int code,
-             const char *code_type ) {
-  last_error.id = id;
-  last_error.message = message;
-  last_error.code = code;
-  last_error.code_type = code_type;
-  error_valid = true;
-}
-
-
 #endif /* __STUMPLESS_COMMON_H */
